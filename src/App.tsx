62d885496d2394ad734a82152ba41baf36bacfb1
dkk <dkkloimwieder@gmail.com>
2022-10-15 15:05:33 -0400

    basic map onmount

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,28 @@
-import type { Component } from 'solid-js';
-
-import logo from './logo.svg';
-import styles from './App.module.css';
+import { Component, onMount } from 'solid-js';
+import { Map, View } from 'ol';
+import { fromLonLat } from 'ol/proj';
+import olms from 'ol-mapbox-style';
 
 const App: Component = () => {
-  return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
-  );
+  const apiKey = import.meta.env.VITE_ARCGIS;
+  const basemapId = 'ArcGIS:Topographic';
+  const basemapURL = `https://basemaps-api.arcgis.com/arcgis/rest/services/styles/${basemapId}/?type=style&token=${apiKey}`;
+  let m: HTMLDivElement | undefined;
+  onMount(() => {
+    console.log(import.meta.env);
+    console.log(m);
+    const map = new Map({
+      target: m
+    });
+    map.setView(
+      new View({
+        center: fromLonLat([-118.805, 34.027]),
+        zoom: 13 // scale: 72223.819286
+      })
+    );
+    olms(map, basemapURL);
+  });
+  return <div style={{ width: '100vw', height: '100vh' }} ref={m} />;
 };
 
 export default App;
